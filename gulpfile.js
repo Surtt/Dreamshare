94a5655c4f45b56a2af33aa3a599b50529b3021e
Alexander Sergeev <fallen2@yandex.ru>
2019-05-11 23:25:45 +0300

    Project start

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-csso');
+const rename = require('gulp-rename');
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var del = require('del');
+const server = require('browser-sync').create();
+
+gulp.task('pug', function() {
+  return gulp.src('src/views/*.pug')
+  .pipe(plumber())
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('style', function() {
+  return gulp.src('src/styles/main.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream());
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/img/**/*.{png,jpg,svg}')
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest('src/img'));
+});
+
+gulp.task('webp', function() {
+  return gulp.src('source/img/**/*.{png,jpg}')
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest('src/img'));
+});
+
+gulp.task('sprite', function() {
+  return gulp.src('src/img/*.svg')
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename('sprite.svg'))
+  .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('serve', function() {
+  server.init({
+    server: 'build/'
+  });
+
+  gulp.watch('src/**/*.pug', gulp.series('pug'));
+  gulp.watch('src/styles/**/*.{scss,sass}', gulp.series('style'));
+  gulp.watch('build/*.html').on('change', server.reload);
+});
+
+gulp.task('copy', function() {
+  return gulp.src([
+    'src/fonts/**/*.{woff,woff2}',
+    'src/img/**',
+    'src/js/**'
+  ], {
+    base: 'src'
+  })
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+  return del('build');
+});
+
+gulp.task('build', gulp.series(
+  'clean',
+  'copy',
+  'style',
+  'sprite',
+  'pug'
+));
